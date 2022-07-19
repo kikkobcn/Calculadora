46575a151d193de4c41dca9ca295189a5e854862
kikkojs <kikkocrypto21@gmail.com>
2022-07-19 18:44:03 +0200

    Proyectos varios

--- calc.js
+++ calc.js
@@ -1,22 +1,30 @@
-let dato1 = parseInt(document.getElementById("op1").value);
-let dato2 = parseInt(document.getElementById("op2").value);
+
 
 function sumar() {
+    let dato1 = parseInt(document.getElementById("op1").value);
+    let dato2 = parseInt(document.getElementById("op2").value);
     let total = dato1 + dato2;
+    console.log(total);
     document.getElementById("resultado").innerHTML = total;
 }
 
 function restar() {
+    let dato1 = parseInt(document.getElementById("op1").value);
+    let dato2 = parseInt(document.getElementById("op2").value);
     let total = dato1 - dato2;
     document.getElementById("resultado").innerHTML = total;
 }
 
 function multi() {
+    let dato1 = parseInt(document.getElementById("op1").value);
+    let dato2 = parseInt(document.getElementById("op2").value);
     let total = dato1 * dato2;
     document.getElementById("resultado").innerHTML = total;
 }
 
 function divi() {
+    let dato1 = parseInt(document.getElementById("op1").value);
+    let dato2 = parseInt(document.getElementById("op2").value);
     let total = dato1 / dato2;
     document.getElementById("resultado").innerHTML = total;
 }
@@ -28,4 +36,7 @@ function borradoTotal() {
     document.getElementById("op2").innerHTML = (document.getElementById("op2").value = " ");
     document.getElementById("resultado").innerHTML=0;
     }
+
+
+
 
---
+++ tabla.js
@@ -0,0 +1,21 @@
+//var h = ee
+//let g = 'hola'
+
+
+function multiplicar(valor) {
+  
+    for (let i of valor) {
+        console.log(i);
+        if (i%2==0) {
+            console.log(i + " es par");
+        }
+
+        else {console.log(i + " es impar");}
+
+    }
+        
+}
+
+    
+lista = [1,5,6,7,8,9,10];
+multiplicar(lista)
